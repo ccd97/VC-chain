22e28af50e487fe51667a600ff252241c7a97ece
CCD-1997 <dcunha.cyprien@gmail.com>
2017-08-25 12:45:46 +0530

    link pages with each other : part-2

--- vc_chain/views.py
+++ vc_chain/views.py
@@ -12,35 +12,65 @@ def dashboardView(request, username):
 
 
 def addProjectView(request, username):
-    return render_to_response('add-project.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+    }
+    return render_to_response('add-project.html', data)
 
 
 def codeEditView(request, username, projectname, branchname, filename):
-    return render_to_response('code-edit.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+        "file": dummy.getFileCodeDummyData(),
+    }
+    return render_to_response('code-edit.html', data)
 
 
 def codeView(request, username, projectname, branchname, filename):
-    return render_to_response('code-view.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+        "file": dummy.getFileCodeDummyData(),
+    }
+    return render_to_response('code-view.html', data)
 
 
 def commitView(request, username, projectname, branchname, commitid):
-    return render_to_response('commit-view.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+        "commit_diff": dummy.getCommitDiffDummyData(),
+    }
+    return render_to_response('commit-view.html', data)
 
 
 def commitsListView(request, username, projectname, branchname):
-    return render_to_response('commits.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+        "commits": dummy.getCommitListDummyData(),
+    }
+    return render_to_response('commits.html', data)
 
 
 def editProfileView(request, username):
-    return render_to_response('edit-profile.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+    }
+    return render_to_response('edit-profile.html', data)
 
 
 def peopleView(request, username):
-    return render_to_response('people.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+        "people": dummy.getPeopleDummyData(),
+    }
+    return render_to_response('people.html', data)
 
 
 def projectExplorerView(request, username, projectname, branchname=None):
-    return render_to_response('project-explore.html')
+    data = {
+        "user": dummy.getUserDummyData(),
+        "project": dummy.getProjectExplorerDummyData(),
+    }
+    return render_to_response('project-explore.html', data)
 
 
 def projectsListView(request, username):
